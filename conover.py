6cfb0444996e56961291229bc348c9f6ff06788c
Doychin Atanasov <ironsmile@gmail.com>
2013-05-02 20:18:20 +0300

    PEP8ish

--- conover.py
+++ conover.py
@@ -3,51 +3,52 @@
 from _winreg import *
 import sys
 
-def check_enabled(aReg):
-	overriden = False
-	print "Checking connection override... ",
-	aKey = OpenKey(aReg, r"Software\Classes\Software\Microsoft\Xlive") 
-	for i in range(1024):                                           
-		try:
-			n,v,t = EnumValue(aKey,i)
-			if 'ConnectionOverride' == n:
-				print "it is on. Interface: %s" % v
-				overriden = v
-				break
-		except EnvironmentError:
-			print "it is off."
-			break          
-	CloseKey(aKey)
-	return overriden
 
-	
+def check_enabled(aReg):
+    overriden = False
+    print "Checking connection override... ",
+    aKey = OpenKey(aReg, r"Software\Classes\Software\Microsoft\Xlive")
+    for i in range(1024):
+        try:
+            n, v, t = EnumValue(aKey, i)
+            if 'ConnectionOverride' == n:
+                print "it is on. Interface: %s" % v
+                overriden = v
+                break
+        except EnvironmentError:
+            print "it is off."
+            break
+    CloseKey(aKey)
+    return overriden
+
+    
 def enable(aReg, what=r'Hamachi'):
-	print "Enabling"
-	aKey = OpenKey(aReg, r"Software\Classes\Software\Microsoft\Xlive", 0, KEY_WRITE)
-	try:   
-	   SetValueEx(aKey,"ConnectionOverride",0, REG_SZ, what) 
-	except EnvironmentError:                                          
-		print "Encountered problems writing into the Registry..."
-	CloseKey(aKey)
-	
+    print "Enabling"
+    aKey = OpenKey(aReg, r"Software\Classes\Software\Microsoft\Xlive", 0, KEY_WRITE)
+    try:
+        SetValueEx(aKey, "ConnectionOverride", 0, REG_SZ, what)
+    except EnvironmentError:
+        print "Encountered problems writing into the Registry..."
+    CloseKey(aKey)
+    
+
 def disable(aReg):
-	print "Disabling"
-	aKey = OpenKey(aReg, r"Software\Classes\Software\Microsoft\Xlive", 0, KEY_WRITE)
-	DeleteValue(aKey, r"ConnectionOverride")
-	CloseKey(aKey)
-	pass
+    print "Disabling"
+    aKey = OpenKey(aReg, r"Software\Classes\Software\Microsoft\Xlive", 0, KEY_WRITE)
+    DeleteValue(aKey, r"ConnectionOverride")
+    CloseKey(aKey)
+    pass
 
 if __name__ == "__main__":
-	aReg = ConnectRegistry(None,HKEY_CURRENT_USER)
-	enabled = check_enabled(aReg)
-	
-	if len(sys.argv) > 1:
-		if "enable" == sys.argv[1] and not enabled:
-			enable(aReg, 'Tunngle' if len(sys.argv) >= 3 and 'Tunngle' == sys.argv[2] else r'Hamachi')
-		elif "disable" == sys.argv[1] and enabled:
-			disable(aReg)
-	else:
-		print "Usage: %s [disable|enable [Hamachi|Tunngle]]" % sys.argv[0]
-		
-	CloseKey(aReg)
-	
+    aReg = ConnectRegistry(None, HKEY_CURRENT_USER)
+    enabled = check_enabled(aReg)
+    
+    if len(sys.argv) > 1:
+        if "enable" == sys.argv[1] and not enabled:
+            enable(aReg, 'Tunngle' if len(sys.argv) >= 3 and 'Tunngle' == sys.argv[2] else r'Hamachi')
+        elif "disable" == sys.argv[1] and enabled:
+            disable(aReg)
+    else:
+        print "Usage: %s [disable|enable [Hamachi|Tunngle]]" % sys.argv[0]
+        
+    CloseKey(aReg)
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 import py2exe
 
 setup(
-	windows=['gui.py'], 
-	options = {
+    windows=['gui.py'],
+    options={
         "py2exe": {
             "dll_excludes": ["MSVCP90.dll"]
         }
